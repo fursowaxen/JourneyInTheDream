195bf05b47d8745e932809f86808253e2cab267a
fursowaxen <144027354+fursowaxen@users.noreply.github.com>
2024-01-09 20:26:28 +0300

    Add files via upload

---
+++ pictures/data/maps/bckg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="bckg" tilewidth="70" tileheight="70" tilecount="400" columns="20">
+ <image source="bckg.png" width="1449" height="1465"/>
+</tileset>
