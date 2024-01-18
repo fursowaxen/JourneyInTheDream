d226c42c204b67702ad838babc0e5080e7a1b275
fursowaxen <144027354+fursowaxen@users.noreply.github.com>
2024-01-18 18:27:47 +0300

    Update use_data.py

--- Forms/use_data.py
+++ Forms/use_data.py
@@ -5,5 +5,5 @@ def save_data(n):
 
 def take_data():
     with open('data/res.txt', 'r+') as file:
-        return max([int(i) for i in file.readlines()])
+        return min([int(i) for i in file.readlines()])
 
